82f3bb27b4e4344e4c7ea4f5ab4d342ec2955bf7
Likit Preeyanon <likit.pre@mahidol.edu>
2020-10-03 14:15:54 +0700

    updated the navbar, index page
    updated the question list page
    
    added a submit view to the webadmin

--- app/webadmin/views.py
+++ app/webadmin/views.py
@@ -1,6 +1,6 @@
 from . import webadmin
 from app.exambank.models import *
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, flash
 
 
 @webadmin.route('/banks')
@@ -20,3 +20,13 @@ def preview(item_id):
     item = Item.query.get(item_id)
     answer = Choice.query.get(item.answer_id)
     return render_template('webadmin/preview.html', item=item, answer=answer)
+
+
+@webadmin.route('/<int:item_id>/submit')
+def submit(item_id):
+    item = Item.query.get(item_id)
+    item.status = 'submit'
+    db.session.add(item)
+    db.session.commit()
+    flash('บันทึกข้อสอบเรียบร้อยแล้ว', 'success')
+    return redirect(url_for('webadmin.list_questions', bank_id=item.bank.id))
