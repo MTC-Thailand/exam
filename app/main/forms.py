ee58c0fea2eadfc905ec1adc3837ed98937945b5
Likit Preeyanon <likit.pre@mahidol.edu>
2020-11-27 05:23:08 +0700

    added a form for a password change and email

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log in')
+
+
+class UserForm(FlaskForm):
+    password = PasswordField('New Password',
+                             validators=[DataRequired()])
+    confirm = PasswordField('Confirm Password',
+                            validators=[DataRequired(),
+                                        EqualTo('password', message='Password must match.')])
+    email = EmailField('E-mail', validators=[Email(), DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, current_user, login_required, logout_user
+from werkzeug.security import generate_password_hash
+
 from . import mainbp as main
-from .forms import LoginForm
+from .forms import LoginForm, UserForm
 from .models import User
+from .. import db
 
 
 @main.route('/')
@@ -41,3 +44,25 @@ def login():
                 return redirect(nextUrl)
 
     return render_template('main/login.html', form=form)
+
+
+@main.route('/user', methods=['GET', 'POST'])
+@login_required
+def user():
+    form = UserForm()
+    if current_user.email:
+        form.email.data = current_user.email
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.password.data == form.confirm.data:
+                current_user.password_hash = generate_password_hash(form.password.data)
+                current_user.email = form.email.data
+                db.session.add(current_user)
+                db.session.commit()
+                flash('New password has been saved.', 'success')
+        else:
+            for field, errors in form.errors.items():
+                flash('{}'.format(', '.join(errors)), 'danger')
+            if not current_user.email:
+                form.email.data = ''
+    return render_template('main/useraccount.html', form=form)
