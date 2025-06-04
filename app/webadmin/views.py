ceaa378fd0f64949a3255d3d2bbc88176e5c205a
Likit Preeyanon <preeyano@msu.edu>
2025-06-04 09:56:46 +0700

    added print request data for testing

--- app/webadmin/views.py
+++ app/webadmin/views.py
@@ -1040,6 +1040,7 @@ def push_to_inet(spec_id, random_set_id):
         'created_datetime': random_set.created_at.isoformat(),
         'items': random_set.to_json(),
     }
+    print(data)
     resp = requests.post(INET_API_BASE_URL, json=data, verify=True)
     if resp.status_code != 200:
         print(resp.text)
